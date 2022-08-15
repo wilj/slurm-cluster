3ad54646b28785f651c690e76bd828fc90c2ba2c
Wil Johnson <15970051+wilj@users.noreply.github.com>
2022-08-15 20:11:17 +0000

    added example job

--- jupyter/docker-entrypoint.sh
+++ jupyter/docker-entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+cp -R /tmp/src/examples /home/admin
+sudo chown -r admin:admin /home/admin
+
 sudo service munge start
 
 jupyter lab --no-browser --allow-root --ip=0.0.0.0 --NotebookApp.token='' --NotebookApp.password=''
