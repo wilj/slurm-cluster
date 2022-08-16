22123b5b28c71da6c99e9c19ecbfff5bde017b97
Wil Johnson <15970051+wilj@users.noreply.github.com>
2022-08-16 19:58:45 +0000

    improved logging

---
+++ reset.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+docker-compose rm -v --force || echo "nothing to remove"
+docker rm $(docker ps -aq) || echo "no containers to remove"
+docker volume rm slurm-cluster_shared-vol || echo "no volume to remove"
+docker-compose build
+docker-compose up
